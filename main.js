e1e89dacec471d45f7d697f3208713f2c4633fe4
unknown <jueeli97@gmail.com>
2020-07-30 22:55:57 +0530

    DataType
    Signed-off-by: unknown <jueeli97@gmail.com>

---
+++ main.js
@@ -0,0 +1,25 @@
+// Datatypes
+
+
+//STATIC TYPE DATATYPE
+
+// NUMBER DATATYPE
+let numValue;// Declaration of variable
+numValue=10;// initialize value
+console.log(numValue);// 10 (output)
+console.log(typeof(numValue));// number
+
+
+// STRING DATATYPE
+let strName;// Declaration of variable
+strName = "Jueeli"; // initialize value
+console.log(strName); // output= Jueeli
+console.log(typeof(strName)); // string
+
+//BOOLEAN DATATYPE
+let boolValue; // Declaration of variable
+boolValue= true; // initialize value 
+console.log(boolValue); // true
+console.log(typeof(boolValue)); // boolean
+
+
